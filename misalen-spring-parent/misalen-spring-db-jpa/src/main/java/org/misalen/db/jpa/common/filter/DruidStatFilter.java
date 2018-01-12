b60a42731ee42553acfb90b8cc020b25c1e09f3d
misalen <misalen@qq.com>
2018-01-12 14:10:30 +0800

    删除不必要模块

--- misalen-spring-parent/misalen-spring-db-jpa/src/main/java/org/misalen/db/jpa/common/filter/DruidStatFilter.java
+++ misalen-spring-parent/misalen-spring-db-jpa/src/main/java/org/misalen/db/jpa/common/filter/DruidStatFilter.java
@@ -15,5 +15,4 @@ import com.alibaba.druid.support.http.WebStatFilter;
 @WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*", initParams = {
 		@WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*") })
 public class DruidStatFilter extends WebStatFilter {
-
 }
